2f7dae8daa08b6cbd5cfbe25c99431063db32354
Alejandro Jr. Nava Aldana <anavaald@uci.edu>
2025-03-15 01:05:33 +0000

    dijkstra issues

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -53,5 +53,5 @@ void print_path(const vector<int>& path, int total) {
         if (i > 0) cout << " ";  // Space instead of "->"
         cout << path[i];
     }
-    cout << " \nTotal cost is " << total << endl;
+    cout << " \nTotal cost is " << total << "\n";
 }
